f9d9c2dd18b177b17aea5749ce7790b4f5779780
Ujjwal Garg <ujjwalgarg.252@gmail.com>
2022-01-16 02:15:42 +0530

    fixed cors

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
